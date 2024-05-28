10ee34e3849871787267e490459d9f3f321b2cd5
RaresScoica <rares.goiceanu@yahoo.com>
2024-05-28 05:51:11 -0700

    SSL and port and...

--- backend/app.py
+++ backend/app.py
@@ -322,4 +322,5 @@ def send_email(attachment_file, transactionId, email):
     server.quit()
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
