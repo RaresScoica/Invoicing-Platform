b9d70bd00b2248bdae18c0037f683c8f52280834
RaresScoica <rares.goiceanu@yahoo.com>
2024-05-29 01:13:10 -0700

    logo

--- backend/app.py
+++ backend/app.py
@@ -84,7 +84,7 @@ def success():
     # Format the datetime object in the desired format
     current_date = parsed_time.strftime("%d/%m/%Y %H:%M")
 
-    with open('../frontend/images/dfg_logo.png', 'rb') as f:
+    with open('/frontend/images/dfg_logo.png', 'rb') as f:
         image_data = f.read()
 
     # Convert image data to base64-encoded string
@@ -305,7 +305,7 @@ def send_email(attachment_file, transactionId, email):
     msg.attach(MIMEText(body, 'html'))
 
     # Attach the logo image
-    with open("../frontend/images/logo_nobg.png", 'rb') as f:
+    with open("/frontend/images/logo_nobg.png", 'rb') as f:
         logo = MIMEImage(f.read(), _subtype="svg+xml")
         logo.add_header('Content-ID', '<logo>')
         msg.attach(logo)
