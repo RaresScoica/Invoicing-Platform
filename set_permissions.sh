b6f51a7510f9d88122f52bd1c73c020b9e90e507
RaresScoica <rares.goiceanu@yahoo.com>
2024-06-26 02:29:16 -0700

    jfd

--- set_permissions.sh
+++ set_permissions.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 chmod +x /opt/render/project/src//wkhtmltopdf.exe
+
 #!/bin/bash
 set -e
-
-# Ensure apt directory structure exists
-mkdir -p /var/lib/apt/lists/partial
 
 # Update package lists
 apt-get update
