99ce4f2b574f720d392b8bf632e05877f94d497a
RaresScoica <rares.goiceanu@yahoo.com>
2024-06-26 02:30:47 -0700

    fdhis

--- set_permissions.sh
+++ set_permissions.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 chmod +x /opt/render/project/src//wkhtmltopdf.exe
 
-#!/bin/bash
-set -e
-
-# Update package lists
-apt-get update
-
-# Install wkhtmltopdf and its dependencies
-apt-get install -y xvfb libfontconfig wkhtmltopdf
-
 # Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
