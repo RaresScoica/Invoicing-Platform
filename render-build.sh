17e818b30c5bdac1cc6a21dbe762b1e6a847bdc7
RaresScoica <rares.goiceanu@yahoo.com>
2024-06-26 06:32:00 -0700

    nifdkf

--- render-build.sh
+++ render-build.sh
@@ -4,13 +4,19 @@
 set -e
 
 # Download precompiled static binary for wkhtmltopdf
-wget -O wkhtmltopdf https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.amd64.static.deb
+wget -O wkhtmltopdf https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
 
 # Make it executable
 chmod +x wkhtmltopdf
 
-# Move it to a known location
-mv wkhtmltopdf /usr/local/bin/wkhtmltopdf
+# Install required libraries
+apt-get update && apt-get install -y libxrender1 libfontconfig1 libxtst6 libjpeg-dev
+
+# Install wkhtmltopdf
+dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb || apt-get -f install -y
+
+# Clean up
+rm wkhtmltox_0.12.6-1.bionic_amd64.deb
 
 # Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
